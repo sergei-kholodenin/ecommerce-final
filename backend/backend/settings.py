f51b2ec3b3ba5af8b2dbf21e9240943ed71c304a
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-05-04 17:29:54 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'frontend/build')
+            os.path.join(BASE_DIR, 'frontend','build')
         ],
         "APP_DIRS": True,
         "OPTIONS": {
