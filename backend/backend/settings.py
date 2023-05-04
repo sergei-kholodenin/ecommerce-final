48488c3123a82c0adf09a4e8c75fb9ac492a78bd
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-05-04 18:18:59 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -199,10 +199,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'templates/static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "frontend/build/static"
+    BASE_DIR / "templates/static"
 ]
 
-MEDIA_ROOT = 'static/images'
+MEDIA_ROOT = 'templates/static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
